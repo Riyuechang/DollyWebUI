84d2c250850c1bcfd8ca24d043771e897a501857
Riyuechang <32575778+Riyuechang@users.noreply.github.com>
2024-04-16 06:24:21 +0800

    Delete log.py
    useless files

--- log.py
+++
@@ -1,8 +0,0 @@
-from loguru import logger
-import sys
-
-logger.remove()
-
-fmt = "<b><g>{time:MM-DD HH:mm:ss}</g> | <lvl>{level:<8}</lvl> | <e>{file}:{line:<4}</e> | <lvl>{message}</lvl></b>"
-
-logger.add(sys.stdout, format=fmt)
