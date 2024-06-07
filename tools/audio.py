c990067acb34d099b58ff5a0ba9ade60b86e5815
Riyuechang <tsps99187@gmail.com>
2024-06-07 15:41:18 +0800

    整理程式碼

--- tools/audio.py
+++ tools/audio.py
@@ -2,8 +2,8 @@ import time
 from io import BytesIO
 from typing import Literal
 
-import pygame
-import pygame._sdl2.audio as sdl2_audio
+from pygame import mixer as pygame_mixer
+from pygame._sdl2 import audio as sdl2_audio
 from pydub import AudioSegment
 from pydub.silence import detect_leading_silence
 
@@ -12,9 +12,9 @@ from tools.log import logger
 
 #取得音訊設備名稱
 def get_audio_device_names() -> list[str]:
-    pygame.mixer.init() #初始化混音器
+    pygame_mixer.init() #初始化混音器
     audio_device_names = sdl2_audio.get_audio_device_names(False) #取得音訊設備名稱
-    pygame.mixer.quit() #退出混音器
+    pygame_mixer.quit() #退出混音器
     return audio_device_names
 
 #初始化音訊
@@ -22,23 +22,23 @@ def initialize_audio(audio_device_names_list: list) -> str:
     default_audio_device = config.default.audio.audio_device_name
 
     if default_audio_device in audio_device_names_list: #判斷默認音訊設備是否在"音訊設備名稱清單"裡
-        pygame.mixer.init(devicename=default_audio_device) #初始化混音器,並指定音訊設備
+        pygame_mixer.init(devicename=default_audio_device) #初始化混音器,並指定音訊設備
         logger.info(f"音訊初始化成功  設備名稱:{default_audio_device}")
         return default_audio_device
     else: #找不到默認音訊設備,則不指定音訊設備
-        pygame.mixer.init() #初始化混音器
+        pygame_mixer.init() #初始化混音器
         logger.info("音訊初始化成功  設備名稱:預設音訊裝置")
         return "預設"
 
 #改變音訊設備
 def change_audio_device(audio_device_name: str):
-    pygame.mixer.quit() #退出混音器
+    pygame_mixer.quit() #退出混音器
 
     if audio_device_name == "預設":
-        pygame.mixer.init() #初始化混音器
+        pygame_mixer.init() #初始化混音器
         logger.info("改變音訊設備成功  設備名稱:預設音訊裝置")
     else:
-        pygame.mixer.init(devicename=audio_device_name) #初始化混音器,並指定音訊設備
+        pygame_mixer.init(devicename=audio_device_name) #初始化混音器,並指定音訊設備
         logger.info(f"改變音訊設備成功  設備名稱:{audio_device_name}")
 
 #播放音訊
@@ -46,11 +46,11 @@ def play_audio(
     voice: bytes, 
     audio_volume: int
 ):
-    sound = pygame.mixer.Sound(BytesIO(voice)) #載入音訊
+    sound = pygame_mixer.Sound(BytesIO(voice)) #載入音訊
     sound.set_volume(audio_volume / 100) #調整音量
     sound.play()
 
-    while pygame.mixer.get_busy(): #等待播放完畢
+    while pygame_mixer.get_busy(): #等待播放完畢
         time.sleep(0.1)
 
 #把pydub格式轉成bytes
